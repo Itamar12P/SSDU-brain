db21673bc82bd68b4e6ee37bebd893b79e688403
Noam Nayman <itamarp1@mail.tau.ac.il>
2026-02-20 13:47:58 +0200

    chore: re-increase epoch time

--- train.py
+++ train.py
@@ -159,7 +159,7 @@ with tf.compat.v1.Session(config=config) as sess:
         except tf.errors.OutOfRangeError:
             pass
 
-        if (np.mod(ep, 1) == 0):
+        if (np.mod(ep, 10) == 0):
             saver.save(sess, sess_trn_filename, global_step=ep)
             sio.savemat(os.path.join(directory, 'TrainingLog.mat'), {'loss': totalLoss})
 
